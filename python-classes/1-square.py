6b9accc75ba534479cc5dcfb22bf156ef6c57e06
zeyneb1pashazade <zeyneb1pashazade@gmail.com>
2025-11-23 16:25:20 +0000

    python

--- python-classes/1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""0-square.py: Defines a square with a private size attribute"""
-
-class Square:
-    """Class that defines a square with a private size attribute"""
-
-    def __init__(self, size):
-        """Initialize the square with a given size"""
-        self.__size = size
